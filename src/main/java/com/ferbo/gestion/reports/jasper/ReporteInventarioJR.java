8c60edf2c00ab587eb3563966f5962bc07fc874d
calberto <rinoa_heartili@hotmail.com>
2026-06-10 16:59:42 -0600

    Se agrega a las consulta de reportes de entradas, salidas, inventarios y ocupacion de camaras la opcion de multi-cliente

--- src/main/java/com/ferbo/gestion/reports/jasper/ReporteInventarioJR.java
+++ src/main/java/com/ferbo/gestion/reports/jasper/ReporteInventarioJR.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -48,10 +49,37 @@ public class ReporteInventarioJR extends AbstractJR {
         
         return bytes;
 	}
-	
-	public byte[] getPDFReporteInventario(Integer idCliente, Integer idPlanta) throws GestionException {
+        
+        public byte[] getPDFReporteInventario(Integer idCliente, Integer idPlanta) throws GestionException {
         return this.getPDFReporteInventario(new Date(), idCliente, idPlanta);
 	}
+        
+        public byte[] getPDFReporteInventario(Date fecha, List<Integer> listClientes, Integer idPlanta) throws GestionException {
+            byte[]              bytes = null;
+            InputStream         jrxml = null;
+            JasperBL jasperBO = new JasperBL();
+
+            try {
+                log.info("Ruta logo: " + this.logoPath);
+                jrxml = this.fsTools.getResourceStream(reportNameJASPER);
+                this.jrParams.put("idCliente", listClientes);
+                this.jrParams.put("Fecha", fecha);
+                this.jrParams.put("Camara", null);
+                this.jrParams.put("Planta", idPlanta);
+                this.jrParams.put("imagen", this.logoPath);
+
+                bytes = jasperBO.createPDF(this.jrParams, jrxml);
+
+            } catch(Exception ex) {
+                throw new GestionException("Problema en el procesamiento del reporte de inventario (PDF)...", ex);
+            }
+
+            return bytes;
+	}
+        
+        public byte[] getPDFReporteInventario(List<Integer> listClientes, Integer idPlanta) throws GestionException {
+            return this.getPDFReporteInventario(new Date(), listClientes, idPlanta);
+        }
 	
 	public byte[] getXLSReporteInventario(Date fecha, Integer idCliente, Integer idPlanta) throws GestionException {
 		byte[]              bytes = null;
@@ -81,9 +109,42 @@ public class ReporteInventarioJR extends AbstractJR {
         
         return bytes;
 	}
-	
-	public byte[] getXLSReporteInventario(Integer idCliente, Integer idPlanta) throws GestionException {
+        
+        public byte[] getXLSReporteInventario(Integer idCliente, Integer idPlanta) throws GestionException {
         return this.getXLSReporteInventario(new Date(), idCliente, idPlanta);
 	}
+        
+        public byte[] getXLSReporteInventario(Date fecha, List<Integer> listClientes, Integer idPlanta) throws GestionException {
+		byte[]              bytes = null;
+            InputStream         jrxml = null;
+            Map<String, Object> jrParams = null;
+
+            JasperBL jasperBO = new JasperBL();
+
+            try {
+                    log.info("Ruta logo: " + this.logoPath);
+                    jrxml = this.fsTools.getResourceStream(reportNameJASPER);
+
+                jrParams = new HashMap<String, Object>();
+                jrParams.put("REPORT_CONNECTION", conn);
+                jrParams.put("imagen", logoPath);
+                jrParams.put("idCliente", listClientes);
+                jrParams.put("Fecha", fecha);
+                jrParams.put("Camara", null);
+                jrParams.put("Planta", idPlanta);
+                jrParams.put("REPORT_LOCALE", new Locale("es", "MX"));
+
+                bytes = jasperBO.createXLSX(jrParams, jrxml);
+
+            } catch(Exception ex) {
+                throw new GestionException("Problema en el procesamiento del reporte de inventario (PDF)...", ex);
+            }
+
+            return bytes;
+	}
+        
+        public byte[] getXLSReporteInventario(List<Integer> listClientes, Integer idPlanta) throws GestionException {
+            return this.getXLSReporteInventario(new Date(), listClientes, idPlanta);
+        }
 
 }
